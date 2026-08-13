cc5fffedfda161f9dfea18c21afeb13c2ce8c559
Shabnam Mehla <mehlashabnam@gmail.com>
2026-08-14 01:53:37 +0530

    Enhance generateWorkerPDF with comments and structure
    Updated the generateWorkerPDF function to include comments and improve clarity.

--- backend/generateWorkerPDF.js
+++ backend/generateWorkerPDF.js
@@ -1,15 +1,20 @@
 import puppeteer from "puppeteer";
 import { workerResumeTemplate } from './templates/workerResumeTemplate.js';
+//uploads/worker_123.pdf--output path
 export default async function generateWorkerPDF(worker, outputPath, language = 'en') {
+  //puppeteer starts a browswer which is headless means not visible on screen
   const browser = await puppeteer.launch({
     headless: "new",
     args: ["--no-sandbox", "--disable-setuid-sandbox"]
   });
-
+//Browser
+// ↓
+//New Tab
   const page = await browser.newPage();
 
   const html = workerResumeTemplate(worker, language);
 
+  //loading generated html into puppeteer browser
   await page.setContent(html, { waitUntil: "networkidle0" });
 
   await page.pdf({
